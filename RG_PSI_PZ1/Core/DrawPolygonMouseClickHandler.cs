c3b12ba043dfeb07d8ff15697d4d86bc046ed6e3
Danilo Novakovic <dakenzi97@gmail.com>
2020-03-26 10:36:57 +0100

    User can now draw polygon and modify it

--- RG_PSI_PZ1/Core/DrawPolygonMouseClickHandler.cs
+++ RG_PSI_PZ1/Core/DrawPolygonMouseClickHandler.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
@@ -21,33 +22,65 @@ namespace RG_PSI_PZ1.Core
 
         public void Handle(MouseButtonEventArgs e)
         {
+            var point = e.GetPosition(_canvas);
+
             if (e.ChangedButton == MouseButton.Right)
             {
-                var point = e.GetPosition(_canvas);
-                _points.Add(point);
-                return;
+                HandleRightClick(point);
             }
-
-            if (e.ChangedButton == MouseButton.Left)
+            else if (e.ChangedButton == MouseButton.Left && _points.Count > 1)
             {
-                var fillColor = Color.FromRgb(0, 0, 255);
-
-                var borderColor = Color.FromRgb(0, 0, 0);
-
-                var polygon = new Polygon
-                {
-                    Points = _points,
-                    Fill = new SolidColorBrush(fillColor),
-                    Stroke = new SolidColorBrush(borderColor),
-                    StrokeThickness = 2
-                };
-
-                _commandManager.Execute(new DrawUIElementCommand(_canvas, polygon, new Point(0, 0)));
-
-                // Don't want to .Clear because it might affect Points in polygon
-                _points = new PointCollection();
-                return;
+                HandleLeftClick(point);
             }
+        }
+
+        private void HandleRightClick(Point point)
+        {
+            _points.Add(point);
+        }
+
+        private void HandleLeftClick(Point point)
+        {
+            var polygon = new Polygon { Points = _points };
+
+            if (ShowPolygonDialog(point, polygon))
+            {
+                AttachEventHandlersToPolygon(polygon);
+                _commandManager.Execute(new DrawUIElementCommand(_canvas, polygon, new Point(0, 0)));
+            }
+
+            // Don't want to .Clear because it might affect Points in polygon
+            _points = new PointCollection();
+        }
+
+        private void AttachEventHandlersToPolygon(Polygon polygon)
+        {
+            polygon.MouseLeftButtonUp += (sender, e) =>
+            {
+                ShowPolygonDialog(e.GetPosition(_canvas), polygonToEdit: (Polygon)sender);
+            };
+        }
+
+        private bool ShowPolygonDialog(Point canvasClickPoint, Polygon polygonToEdit)
+        {
+            Debug.WriteLine("Opening DrawPolygonWindow dialog...");
+
+            var absoluteClickPoint = _canvas.PointToScreen(canvasClickPoint);
+            Debug.WriteLine($"AbsClickPoint: ({absoluteClickPoint.X},{absoluteClickPoint.Y})");
+
+            var window = new DrawPolygonWindow
+            {
+                WindowStartupLocation = WindowStartupLocation.Manual,
+                Left = absoluteClickPoint.X,
+                Top = absoluteClickPoint.Y
+            };
+
+            if (polygonToEdit != null)
+            {
+                window.PolygonInput = polygonToEdit;
+            }
+
+            return window.ShowDialog() ?? false;
         }
     }
 }
---
+++ RG_PSI_PZ1/DrawPolygonWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System.Diagnostics;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace RG_PSI_PZ1
+{
+    /// <summary>
+    /// Interaction logic for DrawPolygonWindow.xaml
+    /// </summary>
+    public partial class DrawPolygonWindow : Window
+    {
+        private Polygon _polygon = new Polygon();
+
+        public Polygon PolygonInput
+        {
+            get => _polygon;
+            set
+            {
+                _polygon = value;
+                FillColorInput.SelectedColor = ((SolidColorBrush)_polygon.Fill)?.Color ?? Color.FromRgb(0, 0, 255);
+                BorderColorInput.SelectedColor = ((SolidColorBrush)_polygon.Stroke)?.Color ?? Color.FromRgb(0, 0, 0);
+                BorderThicknessInput.Value = (int?)_polygon.StrokeThickness;
+            }
+        }
+
+        public DrawPolygonWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine("Cancel button clicked");
+
+            DialogResult = false;
+            Close();
+        }
+
+        private void SubmitButton_Click(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine("Submit button clicked");
+            Debug.WriteLine($"Fill Color: {FillColorInput.SelectedColor}");
+            Debug.WriteLine($"Border Color: {BorderColorInput.SelectedColor}, Border Thickness: {BorderThicknessInput.Text}");
+
+            var fillColor = FillColorInput.SelectedColor ?? Color.FromRgb(0, 0, 255);
+            PolygonInput.Fill = new SolidColorBrush(fillColor);
+
+            var borderColor = BorderColorInput.SelectedColor ?? Color.FromRgb(0, 0, 0);
+            PolygonInput.Stroke = new SolidColorBrush(borderColor);
+
+            PolygonInput.StrokeThickness = BorderThicknessInput?.Value ?? 2;
+
+            DialogResult = true;
+            Close();
+        }
+    }
+}
